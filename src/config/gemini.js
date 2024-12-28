1ec166616439605b9a6d5ededed2b74a4c0a2338
lakgautam <lakgautam21@gmail.com>
2024-12-28 05:30:27 +0530

    file added

---
+++ src/config/gemini.js
@@ -0,0 +1,32 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const apiKey =  "AIzaSyDmtgMIUphbX7W60bJBNx3632mPHagAz5Q";
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 40,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    history: [],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  const response = result.response;
+  console.log(result.response.text());
+  return response.text();
+}
+
+run();
+
+export default run;
